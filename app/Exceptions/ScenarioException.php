15b14484a3e8ee26ee89e04ed68827a99f548e30
Vitaliy Parkhomenko <radvers@gmail.com>
2019-08-21 11:15:05 +0300

    forgot to add new files

--- app/Exceptions/ScenarioException.php
+++ app/Exceptions/ScenarioException.php
@@ -1,10 +1,14 @@
 <?php
 
-
 namespace App\Exceptions;
 
+use Exception;
+use Throwable;
 
-class ScenarioException
+class ScenarioException extends Exception
 {
-
+    public function __construct($message = "", $code = 0, Throwable $previous = null)
+    {
+        parent::__construct('Error in scenario: ' . $message);
+    }
 }
